5267728c268014035f6e0885296afcedcd2252a9
Ayodele David <dayodele89@outlook.com>
2022-07-05 17:58:19 +0100

    Implemented Message and ChatRoom Models to be persisted on being sent

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,7 +2,11 @@ from email import message
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from .models import Message,ChatRoom
+from django.contrib.auth import get_user_model
 
+
+User=get_user_model()
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name=self.scope['url_route']['kwargs']['room_name']
@@ -25,12 +29,23 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         text_data_json=json.loads(text_data)
         message=text_data_json['message']
+        room_name=text_data_json['room_name']
+        user_name=text_data_json['user_name']
+        
+
+        #Create Message for the user sending message
+        user=User.objects.get(username=user_name)
+        chat_room=ChatRoom.objects.get(room_name=room_name)
+        message=Message.objects.create(from_user=user,text_content=message,chat_room=chat_room)
+        message.save()
+
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type':'chat_message',
-                'message':message
+                'message':message,
+                'from_user':user_name
             }
         )
 
@@ -38,8 +53,10 @@ class ChatConsumer(WebsocketConsumer):
 
     def chat_message(self,event):
         message=event['message']
+        from_user=event['from_user']
 
-
+        #send the message to group
         self.send(text_data=json.dumps({
-            'message':message
+            'message':message,
+            'from_user':from_user
         }))
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,33 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+from .models import ChatRoom, User
+
 
 # Create your views here.
 def index(request):
     return render(request,'chat/index.html')
 
+
+
 def room(request,room_name):
-    return render(request,'chat/room.html',{'room_name':room_name})
+
+    if(request.user.is_authenticated):
+        user_name=request.user.username
+        print(user_name)
+        if(room_name=='david_jide'):
+            try:
+                room=ChatRoom.objects.get(room_name=room_name)
+            except ChatRoom.DoesNotExist:
+                chat_room=ChatRoom.objects.create(room_name=room_name)
+                david=User.objects.get(username='david')
+                jide=User.objects.get(username='jide')
+                chat_room.users.add(david)
+                chat_room.users.add(jide)
+                chat_room.save()
+
+            return render(request,'chat/room.html',{'room_name':room_name,'user_name':user_name})
+
+    raise Http404
