7db8ca15a0544bb644580b9d7e602c6f3f95f136
Ayodele David <dayodele89@outlook.com>
2022-07-07 20:10:46 +0100

    Migrated to AsyncWebSockets for optimal performance

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,32 +1,33 @@
 from email import message
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 from .models import Message,ChatRoom
 from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
 
 
 User=get_user_model()
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name=self.scope['url_route']['kwargs']['room_name']
         self.room_group_name=f'chat_{self.room_name}'
 
         #Join group to enable communication between all channels in group
-        async_to_sync(self.channel_layer.group_add)(
+        await(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, code):
+        await(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json=json.loads(text_data)
         print(text_data_json)
         if 'read_pk' in text_data_json:
@@ -35,9 +36,7 @@ class ChatConsumer(WebsocketConsumer):
             user_name=text_data_json['user_name']
 
             print(f'THIS IS PK {pk} {user_name}')
-            message=Message.objects.get(pk=pk)
-            message.read_by=f'{message.read_by}{user_name},'
-            message.save()
+            await self.read_message(pk=pk,user_name=user_name)
 
             
 
@@ -53,13 +52,9 @@ class ChatConsumer(WebsocketConsumer):
         #Create Message for the user sending message
 
             print("HIIII")
-            user=User.objects.get(username=user_name)
-            chat_room=ChatRoom.objects.get(room_name=self.room_name)
-            message_obj=Message.objects.create(from_user=user,text_content=message,chat_room=chat_room)
-
-            message_obj.save()
-
-            async_to_sync(self.channel_layer.group_send)(
+            message_obj=await self.save_message(username=user_name,
+            text_content=message)
+            await(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type':'chat_message',
@@ -70,8 +65,8 @@ class ChatConsumer(WebsocketConsumer):
         )
 
 
-
-    def chat_message(self,event):
+    
+    async def chat_message(self,event):
         message=event['message']
         from_user=event['from_user']
         pk=event['message_pk']
@@ -79,8 +74,22 @@ class ChatConsumer(WebsocketConsumer):
 
 
         #send the message to group
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message':message,
             'from_user':from_user,
             'pk':pk
         }))
+
+    @database_sync_to_async
+    def save_message(self,username,text_content):
+            user=User.objects.get(username=username)
+            chat_room=ChatRoom.objects.get(room_name=self.room_name)
+            message_obj=Message.objects.create(from_user=user,text_content=text_content,chat_room=chat_room)
+            message_obj.save()      
+            return message_obj  
+
+    @database_sync_to_async
+    def read_message(self,pk,user_name):
+            message=Message.objects.get(pk=pk)
+            message.read_by=f'{message.read_by}{user_name},'
+            message.save()
